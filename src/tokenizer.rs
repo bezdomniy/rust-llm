b094ed2be9e3c9f9b6eb4ad1449db6ad38fe472c
Ilia <ilia.shibaev@gmail.com>
2024-11-03 00:07:02 +1100

    encoder working

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,6 @@
 use crate::transformer::Config;
 use crate::utils;
+use core::f32;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
@@ -68,36 +69,70 @@ impl Tokenizer {
         eos: bool,
         config: &Config,
     ) -> Result<Vec<u32>, String> {
-        // let mut prompt_tokens = vec![0u32; prompt.len() + 3];
-        // let mut prompt_tokens = Vec::
-        // let str_buffer = vec![0u8; self.max_token_length * 2 + 3];
-
         let mut prompt_tokens = vec![];
 
         if bos {
             prompt_tokens.push(1);
         }
 
+        if prompt.len() > 0 {
+            let dummy_token = self.token_lookup(&" ".to_string());
+            prompt_tokens.push(dummy_token.unwrap());
+        }
+
         prompt_tokens.extend(prompt.graphemes(true).into_iter().flat_map(|x| {
-            let i = self.token_lookup(&x.to_string());
-            if i.is_some() {
-                // println!("{:?}  {:?}", i, self.vocab.get(i.unwrap() as usize));
-                vec![i.unwrap()]
+            let id = self.token_lookup(&x.to_string());
+            if id.is_some() {
+                vec![id.unwrap()]
             } else {
                 x.as_bytes()
                     .iter()
-                    .map(|&b| {
-                        // println!("None {:?}", self.vocab.get((b + 3) as usize));
-                        (b + 3) as u32
-                    })
+                    .map(|&b| (b + 3) as u32)
                     .collect::<Vec<u32>>()
             }
         }));
 
+        loop {
+            let mut best_score = f32::MIN;
+            let mut best_id = -1i32;
+            let mut best_idx = -1i32;
+
+            prompt_tokens.windows(2).enumerate().for_each(|(i, pair)| {
+                if let &[a, b] = pair {
+                    let merged_str =
+                        format!("{}{}", self.vocab[a as usize], self.vocab[b as usize]);
+                    let id = self.token_lookup(&merged_str.to_string());
+
+                    match id {
+                        Some(id) => {
+                            best_score = self.vocab_scores[id as usize];
+                            best_id = id as i32;
+                            best_idx = i as i32;
+                        }
+                        None => {}
+                    }
+                }
+            });
+
+            if best_idx == -1 {
+                break;
+            }
+
+            prompt_tokens[best_idx as usize] = best_id as u32;
+            prompt_tokens.remove((best_idx + 1) as usize);
+        }
+
+        println!(
+            "{:?}",
+            prompt_tokens
+                .iter()
+                .map(|&i| { self.vocab.get(i as usize).unwrap() })
+                .collect::<Vec<_>>()
+        );
+
         if eos {
             prompt_tokens.push(2);
         }
-        println!("{:?} {:?}", prompt_tokens, prompt_tokens.capacity());
 
         Ok(prompt_tokens)
     }
